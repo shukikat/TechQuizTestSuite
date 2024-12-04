6b06c0f8fa02c621d425741d14fca2afee46da19
shukikat <kathuriashuki@gmail.com>
2024-12-04 00:10:38 -0500

    added component test for start quiz

--- Develop/cypress/component/Quiz.cy.tsx
+++ Develop/cypress/component/Quiz.cy.tsx
@@ -1,9 +1,20 @@
+import {mount} from 'cypress/react'
 import React from 'react'
 import Quiz from '../../client/src/components/Quiz'
 
-describe('<Quiz />', () => {
-  it('renders', () => {
+
+describe('Quiz', () => {
+  it('should render the "Start Quiz" button', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Quiz />)
+     mount(<Quiz />)
+     cy.get('button').contains('Start Quiz').should('be.visible')
+  })
+
+  it('quiz will start when button is clicked', () => {
+       mount(<Quiz/>)
+       cy.get('button').contains('Start Quiz').should('be.visible')
+       cy.get('button').contains('Start Quiz').click()
+       cy.get('button').contains('Start Quiz').should('not.exist')
+
   })
 })
